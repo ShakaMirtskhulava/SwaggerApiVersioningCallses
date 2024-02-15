aec958991e45235fb3e92d8d309dc271cb4368be
Shaka Mirtskhulava <102857931+ShakaMirtskhulava@users.noreply.github.com>
2024-02-15 12:58:36 +0400

    Add files via upload

---
+++ ConfigureSwaggerOptions.cs
@@ -0,0 +1,46 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace WEBApiOrigin2.Versioning;
+
+public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider _apiVersionDescriptionProvider;
+
+    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider apiVersionDescriptionProvider)
+        => _apiVersionDescriptionProvider = apiVersionDescriptionProvider;
+
+    public void Configure(SwaggerGenOptions options)
+    {
+        foreach (var description in _apiVersionDescriptionProvider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(description.GroupName, CreateOpenApiInfo(description));
+        }
+    }
+
+    private static OpenApiInfo CreateOpenApiInfo(ApiVersionDescription description)
+    {
+        var info = new OpenApiInfo()
+        {
+            Title = "Person Api",
+            Version = description.ApiVersion.ToString(),
+            Description = "Api to manage Persons",
+            Contact = new OpenApiContact
+            {
+                Email = "itacademy@tbcbank.com",
+                Name = "TBC IT Academy",
+                Url = new Uri("https://tbcacademy.ge")
+            }
+        };
+
+        if (description.IsDeprecated)
+        {
+            info.Description += " (deprecated)";
+        }
+
+        return info;
+    }
+
+}
